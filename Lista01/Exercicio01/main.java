67dbe261e3b8c0282b1bc864b8bfc2ececfa17b1
Gabriel Couto <coutogabriel427@gmail.com>
2025-09-01 16:57:34 -0300

    Update main.java

--- Lista01/Exercicio01/main.java
+++ Lista01/Exercicio01/main.java
@@ -1,1 +1,18 @@
+package Exercicio01;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira um número: ");
+        int numero1 = scanner.nextInt();
+        System.out.print("Insira o segundo número: ");
+        int numero2 = scanner.nextInt();
+        int resultado = numero1 + numero2;
+        System.out.println(numero1 + " + " + numero2 + " = " + resultado);
+      scanner.close();
+    }
+}
 
